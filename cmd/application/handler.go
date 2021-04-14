f1d9bce83f3c3298692086ac3dc76c213bdca7d3
Seungkyu Ahn <seungkyua@gmail.com>
2021-04-14 17:28:09 +0900

    Make boilerplate code

---
+++ cmd/application/handler.go
@@ -0,0 +1,63 @@
+package application
+
+import (
+	"context"
+
+	"github.com/sktelecom/tks-contract/pkg/log"
+	pb "github.com/sktelecom/tks-proto/pbgo"
+)
+
+type Server struct {
+	pb.UnimplementedAppInfoServiceServer
+}
+
+func (*Server) GetAppID(ctx context.Context, req *pb.GetAppRequest) (*pb.IDsResponse, error) {
+	clusterId := req.GetClusterId()
+	log.Info("clusterId: ", clusterId)
+
+	ids := []string{"111", "222"}
+	res := &pb.IDsResponse{
+		Ids: ids,
+	}
+
+	log.Info("AppIds: ", ids)
+	return res, nil
+}
+
+func (*Server) GetAllApps(ctx context.Context, req *pb.IDRequest) (*pb.GetAppsResponse, error) {
+	clusterId := req.GetId()
+	log.Info("clusterId: ", clusterId)
+
+	res := &pb.GetAppsResponse{}
+	return res, nil
+}
+
+func (*Server) GetApps(ctx context.Context, req *pb.GetAppsRequest) (*pb.GetAppResponse, error) {
+	appId := req.GetAppId()
+	log.Info("appId: ", appId)
+
+	res := &pb.GetAppResponse{}
+	return res, nil
+}
+
+func (*Server) UpdateAppStatus(ctx context.Context, req *pb.UpdateAppStatusRequest) (*pb.SimpleResponse, error) {
+	clusterId := req.GetClusterId()
+	log.Info("clusterId: ", clusterId)
+
+	appId := req.GetAppId()
+	log.Info("appId: ", appId)
+
+	res := &pb.SimpleResponse{}
+	return res, nil
+}
+
+func (*Server) UpdateEndpoints(ctx context.Context, req *pb.UpdateEndpointsRequest) (*pb.SimpleResponse, error) {
+	clusterId := req.GetClusterId()
+	log.Info("clusterId: ", clusterId)
+
+	appId := req.GetAppId()
+	log.Info("appId: ", appId)
+
+	res := &pb.SimpleResponse{}
+	return res, nil
+}
---
+++ examples/application/client.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	"github.com/sktelecom/tks-contract/pkg/log"
+	"github.com/sktelecom/tks-info/pkg/cert"
+	pb "github.com/sktelecom/tks-proto/pbgo"
+)
+
+var (
+	port   = flag.Int("port", 9111, "The gRPC server port")
+	tls    = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+	caFile = flag.String("ca_file", "", "The TLS ca file")
+)
+
+func main() {
+	log.Info("Hello I'm a application client")
+
+	opts := grpc.WithInsecure()
+	if *tls {
+		if *caFile == "" {
+			*caFile = cert.Path("x509/ca.crt")
+		}
+		creds, err := credentials.NewClientTLSFromFile(*caFile, "")
+		if err != nil {
+			log.Fatal("Error while loading CA trust certificate: ", err)
+			return
+		}
+		opts = grpc.WithTransportCredentials(creds)
+	}
+
+	addr := fmt.Sprintf(":%d", *port)
+	cc, err := grpc.Dial(addr, opts)
+	if err != nil {
+		log.Fatal("could not connect: ", err)
+	}
+	defer cc.Close()
+
+	c := pb.NewAppInfoServiceClient(cc)
+
+	req := &pb.GetAppRequest{
+		ClusterId: "uuid",
+	}
+	res, err := c.GetAppID(context.Background(), req)
+	if err != nil {
+		log.Fatal("error while calling GetAppId RPC", err)
+	}
+	log.Info("Response from GetAppId: ", res.GetIds())
+}
