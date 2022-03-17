ce465f3a0f3e22807944f84dce2dae46a95a3083
Robert Choi <taeilchoi1@gmail.com>
2022-03-17 12:37:43 +0000

    adjust indent

--- pkg/cluster/model/cluster.go
+++ pkg/cluster/model/cluster.go
@@ -1,20 +1,20 @@
 package model
 
 import (
-	"time"
+  "time"
 
-	uuid "github.com/google/uuid"
-	pb "github.com/openinfradev/tks-proto/tks_pb"
-	"gorm.io/gorm"
+  uuid "github.com/google/uuid"
+  pb "github.com/openinfradev/tks-proto/tks_pb"
+  "gorm.io/gorm"
 )
 
 // Cluster represents a kubernetes cluster information.
 type Cluster struct {
-	ID             uuid.UUID `gorm:"primarykey;type:uuid;default:uuid_generate_v4()"`
-	Name           string
-	ContractID     uuid.UUID
-	CspID          uuid.UUID
-	Status         pb.ClusterStatus
+  ID             uuid.UUID `gorm:"primarykey;type:uuid;default:uuid_generate_v4()"`
+  Name           string
+  ContractID     uuid.UUID
+  CspID          uuid.UUID
+  Status         pb.ClusterStatus
   SshKeyName     string
   Region         string
   NumOfAz        int32
@@ -22,11 +22,11 @@ type Cluster struct {
   MinSizePerAz   int32
   MaxSizePerAz   int32
   Kubeconfig     string
-	UpdatedAt      time.Time
-	CreatedAt      time.Time
+  UpdatedAt      time.Time
+  CreatedAt      time.Time
 }
 
 func (c *Cluster) BeforeCreate(tx *gorm.DB) (err error) {
-	c.ID = uuid.New()
-	return nil
+  c.ID = uuid.New()
+  return nil
 }
